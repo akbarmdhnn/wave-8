3c2f39dd1222c571154d6df9255230c5d072d0fc
Akbar Ramadhan <akbaramadhnnn@gmail.com>
2025-10-15 21:51:01 +0700

    1

---
+++ 1-express/index.js
@@ -0,0 +1,75 @@
+// import dependencies
+//
+
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { GoogleGenAI } from "@google/genai";
+
+// inisialisasi app
+//
+// deklasrasi variable di JavaScript
+// [const | let] nama_variable = [value]
+// [var] --> 
+//app = "budi"; // error karena const tidak bisa di re-assign
+
+const app = express();
+const upload = multer(); // akan dgunakan dalam recording
+
+const ai = new GoogleGenAI({ }); // instantion menjadi object intance (oop -- object oriented programming)
+
+// middleware
+//
+// contoh penggunaan middleware
+
+app.use(cors()); //inisiasi cors  sebagai middleware
+
+// inisialisasi routing
+// contoh routing: app.get, app.post, app.put, dll
+
+app.post('/generate-text', async (req, res) => {
+    // terima jeroannya, lalu cek disini
+    const { prompt } = req.body;  // object destructuring
+    //satpamnya 
+    if (!prompt || typeof prompt !== 'string') { // cek apakah prompt ada isinya
+    res.status(400).json({
+        success: false,
+        message: 'Prompt harus berupa string!',
+        data: null
+    });
+    }
+
+
+    try { 
+        const aiResponse = await ai.models.generateContent({
+            model: 'gemini-2.5-flash',
+            contents: [
+                { text: prompt }
+            ],
+            config: {
+                systemInstruction: 'Harus di bls bahasa jawa.'
+            }
+        });
+
+
+        res.status(200).json({
+            success: true,
+            message: 'Berhasil dijawab gemini',
+            data: aiResponse.text
+        });
+    } catch (e) { }
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: 'Gagal mendapatkan respon dari Gemini',
+            data: null
+        });
+});
+
+
+// jalankan server
+app.listen(3000, () => {
+    console.log('Ini berjalan di port 3000');
+});
+
+
